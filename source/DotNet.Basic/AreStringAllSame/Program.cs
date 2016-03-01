ff6f9e835613b36d8ed9fa31d6825268e84c4613
atwind <atwind@cszi.com>
2016-03-01 15:06:23 +0800

    字符串的指针获取与对比
    字符串的指针获取与对比

---
+++ source/DotNet.Basic/AreStringAllSame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreStringAllSame
+{
+    /// <summary>
+    /// 所有的字符串的内存位都相同？
+    /// </summary>
+    unsafe class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("所有的字符串的内存位都相同？");
+
+            Console.WriteLine("同一程序域的情况：");
+            var a = "test";
+            var b = "test";
+
+            fixed (char* ap = a)
+            {
+               Console.WriteLine((uint)ap);
+            }
+            fixed (char* ap = b)
+            {
+                Console.WriteLine((uint)ap);
+            }
+
+
+
+
+            Console.Read();
+        }
+    }
+}
