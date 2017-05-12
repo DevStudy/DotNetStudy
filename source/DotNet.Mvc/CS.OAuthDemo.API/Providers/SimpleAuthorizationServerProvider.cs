22f9a685b8972b4de8f81b5cee5be91d1a3eb5d4
PC-01\Atwind <atwind@cszi.com>
2017-05-12 18:43:53 +0800

    待验证OAuth生成的access_token存放在什么地方 AngularJS Token Authentication using ASP.NET Web API 2, Owin, and Identity Step 1: Download Third Party Libraries Step 2: Organize Project Structure Step 3: Add the Shell Page (index.html)

---
+++ source/DotNet.Mvc/CS.OAuthDemo.API/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using CS.OAuthDemo.API.DbAccess;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+
+namespace CS.OAuthDemo.API.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (var _repo = new AuthDao())
+            {
+                IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+#if DEBUG
+
+            //context.OwinContext.Response.Headers.Add("DEBUG", new [] { $"context.Options.AuthenticationType:{context.Options.AuthenticationType}" } );
+
+
+#endif
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+
+        }
+    }
+}
---
+++ source/DotNet.Mvc/CS.OAuthDemo.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的常规信息通过下列特性集
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("CS.OAuthDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS.OAuthDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+// 对 COM 组件不可见。如果需要
+// 从 COM 访问此程序集中的某个类型，请针对该类型将 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+[assembly: Guid("228a888c-b95a-4736-8f62-d18f9c89c9a4")]
+
+// 程序集的版本信息由下列四个值组成:
+//
+//      主版本
+//      次版本
+//      内部版本号
+//      修订版本
+//
+// 可以指定所有值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用 "*":
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+/*
+ 
+step1:
+
+Install-Package Microsoft.AspNet.WebApi.Owin -Version 5.1.2
+Install-Package Microsoft.Owin.Host.SystemWeb -Version 2.1.0     
+
+ The  package “Microsoft.Owin.Host.SystemWeb” is used to enable our Owin server to run our API on IIS using ASP.NET request pipeline as eventually we’ll host this API on Microsoft Azure Websites which uses IIS.    
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     */
